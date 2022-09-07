2a3eb2e12ea0026bb73df6d1c2ad6fe0fc396887
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-07 09:58:27 +0200

    nullish logic added

--- class-notes/DE-02-Variables/2-operators.js
+++ class-notes/DE-02-Variables/2-operators.js
@@ -39,3 +39,56 @@ console.log("c nin degeri : ", ++c, c);
 
 let d = ++c; //Burda direk alir. Ama eger sagina koysaydik alttaki dnin degeri 11 olurdu
 console.log(d);
+
+//?Bazı fonksiyonlar
+// Math.floor();  //* her zaman en yakın alt tamsayıya yuvarlar
+// Math.ceil();  //* her zaman en yakın üst tam sayıya yuvarlar
+// Math.trunc(); //* sayının tam kısmını alır.
+// Math.round(); //* en yakın tam sayıya yuvarlar.
+
+// Math.random(); //* 0 ve 1 arasında rasgele sayı üretir.
+
+//Bir sayinin birler onlar ve yüzler basamagini bulma
+
+const number = 571;
+
+const birler = number % 10; //1
+const onlar = Math.floor((number / 10) % 10); //7
+
+console.log(birler, onlar);
+console.log("merhaba");
+
+// * =======================================================
+// *                 KARŞILAŞTIRMA OPERATÖRLERİ
+// * =======================================================
+
+const sayi1 = 4;
+
+console.log(sayi1 == 4); //true
+console.log(sayi1 == "4"); //true ---->>  == isareti sekle bakiyor.Onun icin ture ciktisi verdi !!!!!
+console.log(sayi1 === "4"); //false --->> === isareti verinin tipine de bakar. Bu sebeple false verdi
+
+// * =======================================================
+// *                TİP DÖNÜŞÜMLERİ
+// * =======================================================
+
+const para = Number("1000") + Number("900"); //1900 dür 1000900 olarak vemez cevabi. Number yazmasaydik öyle verirdi.
+
+console.log(para);
+
+// number yerine alternatif parseInt te kullanılabilir
+// console.log(parseInt(para));
+console.log(Number("")); //0
+console.log(Number(null)); //0
+
+//* -------- NULLISH COALESCING OPERATOR----------- */
+
+//?  Bir ifadenin null veya undefined olması durumuna göre seçim yapmamıza olanak sağlayan bir operatördür.eğer kullanıcı input a null girer yada hiçbişey (undefined) girmezse, belirteceğim ifadeyi (noname) kabul et
+const isim1 = null; // ?null
+const defaultIsim = "noname";
+const islem1 = isim1 ?? defaultIsim;
+// isim1 true ise onu döndür yoksa 2. ifadeyi
+console.log(islem1); //noname
+
+//Eger isim1 yani ilk eleman false ise ikinciyi yaz. Eger ilki(isim1) true ise ilkini yazdir.
+//Örnegin kisi ismini girmedi, null yerine bizim verdigimiz default deger alinsin mantigiyla
