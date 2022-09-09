bbc487c0d06f588efe531bb881744aafa25333dc
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-09 13:21:18 +0200

    number guess completed

--- class-notes/assignments/03-loops/soru2.js
+++ class-notes/assignments/03-loops/soru2.js
@@ -1,37 +1,42 @@
-const randomSayi = Math.ceil(Math.random() * 20);
-console.log(randomSayi);
+function oyun() {
+  const randomSayi = Math.ceil(Math.random() * 20);
+  console.log(randomSayi);
 
-tahmin();
-function tahmin() {
-  let tekrarCevap;
-  let tahmin;
-  let hak = 5;
-  while (hak > 0) {
-    tahmin = +prompt("Tahmininizi giriniz.");
-    if (tahmin < randomSayi) {
-      hak--;
-      if (hak == 0) {
-        alert("HAKKINIZ BITTI MAALESEF");
-        break;
+  function tahmin() {
+    let cevap;
+    let tahmin;
+    let hak = 5;
+    while (hak > 0) {
+      tahmin = +prompt("Tahmininizi giriniz.");
+      if (tahmin < randomSayi) {
+        hak--;
+        if (hak == 0) {
+          alert("HAKKINIZ BITTI MAALESEF");
+          break;
+        } else {
+          alert(`${hak} hakkiniz kaldi. SAYIYI ARTIR⬆️`);
+        }
+      } else if (tahmin > randomSayi) {
+        hak--;
+        if (hak == 0) {
+          alert("HAKKINIZ BITTI MAALESEF");
+          break;
+        } else {
+          alert(`${hak} hakkiniz kaldi. SAYIYI AZALT⏬`);
+        }
       } else {
-        alert(`${hak} hakkiniz kaldi. SAYIYI ARTIR⬆️`);
-      }
-    } else if (tahmin > randomSayi) {
-      hak--;
-      if (hak == 0) {
-        alert("HAKKINIZ BITTI MAALESEF");
-        break;
-      } else {
-        alert(`${hak} hakkiniz kaldi. SAYIYI AZALT⏬`);
-      }
-    } else {
-      alert("TEBRIKLER SAYIYI BILDINIZ 🥳");
-      tekrarCevap = prompt("Oyunu tekrar oynamak ister misiniz ? (E/H)");
-      if (tekrarCevap == "E" || tekrarCevap == "e") {
-        continue;
-      } else {
+        alert("TEBRIKLER SAYIYI BILDINIZ 🥳");
         break;
       }
     }
+    cevap = prompt("Oyunu tekrar oynamak ister misiniz ?(E/H)");
+    if (cevap == "E" || cevap == "e") {
+      oyun();
+    } else {
+      alert("OYUNU SONLANDIRDINIZ. OYUNUMUZU OYNADIGINIZ ICIN TESEKKÜR EDERIM");
+    }
   }
+  tahmin();
 }
+
+oyun();
