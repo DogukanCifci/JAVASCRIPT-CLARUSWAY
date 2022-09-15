dde81dc6d569286100efd7050af8f1fd96855037
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-15 11:47:08 +0200

    changing of the objects added

--- class-notes/DE-08-Object/object.js
+++ class-notes/DE-08-Object/object.js
@@ -84,3 +84,50 @@ console.log(Mensch.özet()); //Johny is 33 years old.
 console.log(Mensch);
 
 //!NOTE: arrow functions ın sözcüksel bağlamı vardır. Yani this keyword ü bir arrow function içinde kullanırsak beklenmedik değerler alabiliriz. Bir object in içindeki this keyword ü, global scope u (window) ifade ettiği için. this value nun global scope a bağlanmasını önlemek için, object yöntemi içindeki normal function u kullanın.
+
+// * ======================================================
+// *                  OBJECT ITERATION
+// * ======================================================
+// key lerin belirli bir sırası (index) olmadığı için object araması daha tutarlı bir arama performansına sahip olacaktır. Ayrıca diziler arasında döngü yapmak, keys arasında döngü yapmaktan daha hızlıdır, bu nedenle tüm öğeler üzerinde işlem yapmayı planlıyorsanız, bunları bir diziye koymak akıllıca olabilir.
+const people = [
+  {
+    name: "Mustafa",
+    surname: "Gertrud",
+    job: "developer",
+    age: 30,
+  },
+  {
+    name: "Halo",
+    surname: "Müller",
+    job: "tester",
+    age: 35,
+  },
+  {
+    name: "Mehmet",
+    surname: "Rosenberg",
+    job: "team lead",
+    age: 40,
+  },
+  {
+    name: "Ozkul",
+    surname: "Gutenberg",
+    job: "developer",
+    age: 26,
+  },
+
+  {
+    name: "Baser",
+    surname: "Shaffer",
+    job: "tester",
+    age: 24,
+  },
+];
+console.log(people);
+
+//örnek1 - people dizisindeki job'lari göster
+
+people.forEach((a) => console.log(a.name, a["job"]));
+
+//Kalici olarak orijinal listeyi degistirmek istersem ;
+people.map((a, i, array) => (array[i].age = a.age + 1));
+console.log(people);
