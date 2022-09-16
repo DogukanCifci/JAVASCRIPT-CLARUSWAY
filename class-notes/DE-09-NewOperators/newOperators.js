3cb2cab3a1e7933724ffad1370ff63e37b7b2f56
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-16 10:31:15 +0200

    newOperators with a function

--- class-notes/DE-09-NewOperators/newOperators.js
+++ class-notes/DE-09-NewOperators/newOperators.js
@@ -40,3 +40,84 @@ console.log(title1, engine1, model1); //BMW 1990 1.6
 const { title1: baslik1, model1: tür1, engine1: motor1 } = car2;
 
 console.log(baslik1, tür1, motor1); //Mercedes 1980, 2.0
+
+//Bu sekilde komple yazdirabiliriz.OBJECT OBJECT ICINDE GEZINME
+for (let i in arabalar) {
+  console.log(arabalar[i]);
+}
+
+//!!!Ama burda for of ve forEach gibi metodlar kullanilamaz. Burda sadece for in ve normal for kullanilabilir.Cünkü objectler iterable degil
+
+//////****************ANOTHER EXAMPLE */
+//* EXAMPLE: Array-Object Icinde Gezinme !!!This is not OBJECT-OBJECT!!!
+console.log("Array-Object Example");
+const people = [
+  {
+    name: "Mustafa",
+    surname: "Gertrud",
+    job: "developer",
+    age: 30,
+  },
+  {
+    name: "Halo",
+    surname: "Müller",
+    job: "tester",
+    age: 35,
+  },
+  {
+    name: "Mehmet",
+    surname: "Rosenberg",
+    job: "team lead",
+    age: 40,
+  },
+  {
+    name: "Ozkul",
+    surname: "Gutenberg",
+    job: "developer",
+    age: 26,
+  },
+
+  {
+    name: "Baser",
+    surname: "Shaffer",
+    job: "tester",
+    age: 24,
+  },
+];
+
+//1.Yöntem for of (for in veya normal for) kullanabiliriz. Cünkü bu bir dizi
+for (let i of people) {
+  console.log(i);
+}
+
+//2.Yöntem forEach() kullanabilirim
+
+people.forEach((item) => {
+  console.log(item.name, item.age, item.job);
+  //veya 2.yöntemde yine newOperators kullanabilirim
+
+  console.log("newOperators ile yazimi(-------------)");
+  const { name, surname, job, age } = item; //item dedim cünkü her 1 elemani item temsil ediyor. Tüm döngüye yazamam
+
+  console.log(surname, job, age);
+});
+
+//Fonksiyonlarda destructing nasil yapiliyor ?
+
+const yazdir = (b) => {
+  const { id, name } = b;
+  console.log(id, name);
+};
+
+const a = { id: 1, name: "Muhammed" };
+
+yazdir(a);
+
+//ama bunu icerde parcalayacagimiza direk fonsiyonun paramaetre yerinde yapabilirim.
+console.log("*************Daha kisa yolla ****");
+
+const yazdir1 = ({ id, name }) => {
+  console.log(id, name);
+};
+const a1 = { id: 1, name: "Muhammed" };
+yazdir1(a1);
