b071cfb5f11de0cf5d1a6bfd0744863b6b9cf544
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-30 09:48:32 +0200

    asyncron logic added

---
+++ class-notes/DE-13-ASYNC/1-timeout_interval.js
@@ -0,0 +1,31 @@
+//*=================================================================
+//*              1- Senkron-Asenkron Programlama
+//* ================================================================
+
+//! Javascript, bir single-threaded ve Asenkron Programlama dilidir.
+
+//? Asenkron Programlama
+//? --------------------------------------------------------------
+//? Asenkron Programlama, bir alt gorevin uygulamanin asil thread'inden
+//? bagimsiz olarak arka planda calistirilmasina izin veren paralel programlama
+//? teknigidir. Bu alt gorev tamamlandiginda (basariyla veya basarisizlikla)
+//? asil thread bu konuda bilgilendirilir. Asenkron programlama, uygulamalarin
+//? performansininin artirilmasina ve daha iyi kullanici deneyimine katki saglamaktadir.
+
+//? Ozellikle bir API'den veya Veritabanindan veri cekme, Giris/Cikis islemleri,
+//? Dosya Okuma/Yazma islemleri gibi zaman tuketen kodlarda Async Programlama
+//? kullanilmasi cok onemlidir.
+
+//*Senkron Programlama
+//************************************** */
+
+const bekle = (ms) => {
+  const start = new Date().getTime();
+
+  while (new Date().getTime() < start + ms) {} //3 saniye bos döndürdü
+};
+console.time("timer1");
+console.log("hello"); //Hemen yazar
+bekle(3000);
+console.timeEnd("timer1");
+console.log("Naber"); //3 saniye sonra yazar
