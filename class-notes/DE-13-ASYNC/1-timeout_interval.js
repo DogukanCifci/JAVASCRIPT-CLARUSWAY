e8459695ffa45bad4672fca6acb5240cf41a759b
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-30 10:36:13 +0200

    setInterval and callback hell added

--- class-notes/DE-13-ASYNC/1-timeout_interval.js
+++ class-notes/DE-13-ASYNC/1-timeout_interval.js
@@ -19,7 +19,7 @@
 //*Senkron Programlama
 //************************************** */
 
-const bekle = (ms) => {
+/* const bekle = (ms) => {
   const start = new Date().getTime();
 
   while (new Date().getTime() < start + ms) {} //3 saniye bos döndürdü
@@ -28,4 +28,50 @@ console.time("timer1");
 console.log("hello"); //Hemen yazar
 bekle(3000);
 console.timeEnd("timer1");
-console.log("Naber"); //3 saniye sonra yazar
+console.log("Naber"); //3 saniye sonra yazar  */
+
+//************************************** */
+//************************************** */
+//*Asenkron (setTimeout) 1 seferlik
+//************************************** */
+//************************************** */
+
+setTimeout(() => {
+  //non-blocking code---macrotask kuyruguna girer
+
+  console.log("Selamun aleyküm");
+}, 2000); //süslüden hemen sonra yazdigim, kac saniye sonra ilacagi anlamina geliyor.ms cinsinden. //Süre  burda 0 olsa bile önce console.log icindeki yazar. Cünkü bir kere buna settimeout atadik.Yani git sirada bekle dedik.Önce sirada beklemeyeceklerin bitmesi lazim.
+
+console.log("Aleyküm selam"); //Kuyrudaga daha sonra girmesine ragmen selamun aleykümden önce yazar
+
+setTimeout(() => {
+  //non-blocking codemacrotask kuyruguna girer
+
+  console.log("Naber napiyosun dostum");
+}, 4000); //Eger sürem bir önceki setTimeOut 'un süresinden daha az olursa önce bu yazar
+
+//*Asenkron(setInterval, clearInterval) belli araliklarla durdurulana kadar devam eder. Mesela 1 dakikada bir döviz kurunu güncellemek icin kullanilabilir
+
+//********SETINTERVAL NONBLOCKING */
+
+console.log("Counter Started");
+let counter = 0;
+
+const myInterval1 = setInterval(() => {
+  ++counter;
+  console.log(counter);
+  if (counter === 5) {
+    clearInterval(myInterval1); //setInterval olayini bitirmek icin clearInterval kullanilir
+  }
+}, 1000); //Her bir saniyede icerdeki olay gerceklessin
+
+//********Callback Hell */
+setTimeout(() => {
+  console.log("ayse:s.a. ahmet");
+  setTimeout(() => {
+    console.log("ahmet:a.s. ayse nasılsın");
+    setTimeout(() => {
+      console.log("ayse:iyiyim sağol");
+    }, 1000);
+  }, 1000);
+}, 1000);
