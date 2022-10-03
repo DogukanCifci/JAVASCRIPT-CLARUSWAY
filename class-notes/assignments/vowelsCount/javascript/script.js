500af500d95a8ad25893e817ed0fb50f9c715e51
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-03 16:53:13 +0200

    vowels Counter completed

--- class-notes/assignments/vowelsCount/javascript/script.js
+++ class-notes/assignments/vowelsCount/javascript/script.js
@@ -3,19 +3,53 @@
 const metin = document.querySelector("#vowels");
 const button = document.querySelector("button");
 const display = document.querySelector(".display");
+let array = [];
 
+let counter = 0;
 const pressDisplay = () => {
-  display.innerHTML = `<p> There are ... vowels in ${metin.value}</p>`;
+  if (counter == 1)
+    display.innerHTML = `<p> There is ${counter} vowels in "${metin.value}" </p>`;
+  else
+    display.innerHTML = `<p> There are ${counter} vowels in "${metin.value}" </p>`;
 };
 
 button.onclick = () => {
-  pressDisplay();
   arrayCevirme();
+  pressDisplay();
+  sifirla();
 };
 
 //Sesli harf sayisini bulma
-let counter = 0;
+
 function arrayCevirme() {
   //Yukarda cagirirken yani daha önce cagirdigimizd icin sikinti olmamasini istiyorsak 3 fonksiyon tütü icinden bunu secmeliyiz
-  let array = metin.value.split("");
+  array = metin.value.split("");
+
+  array.forEach((letter) => {
+    if (
+      letter == "a" ||
+      letter == "e" ||
+      letter == "ı" ||
+      letter == "i" ||
+      letter == "o" ||
+      letter == "ö" ||
+      letter == "u" ||
+      letter == "ü"
+    ) {
+      counter++;
+    }
+  });
 }
+
+//Ard arda 2 defa basilirsa onu düzeltmek icin
+function sifirla() {
+  array = [];
+  counter = 0;
+}
+
+//Entera basildiginda algilamasi icin
+metin.onkeydown = (tus) => {
+  if (tus.keyCode === 13) {
+    document.querySelector("button").click();
+  }
+};
