0b2241537359da432afae2c039f806952332ea1f
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-08 12:51:20 +0200

    new FilteredList created with filter method

---
+++ class-notes/DE-15API-Meals/app1.js
@@ -0,0 +1,41 @@
+const display = document.querySelector(".row");
+let myArray = [];
+const inputBox = document.querySelector(".form-control");
+//Veriyi API'den cekme
+const getMealDataFromApi = async () => {
+  //https request url
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
+  const response = await fetch(url).then((data) => data.json());
+  //console.log(response);
+  myArray = response.meals;
+  console.log(myArray);
+
+  pressDisplay(myArray);
+  searchFood();
+};
+
+getMealDataFromApi();
+
+//Ekrana Bastirma
+
+const pressDisplay = (myArray) => {
+  myArray.forEach((e) => {
+    //Destructiring
+    const { strMeal, strMealThumb } = e;
+
+    display.innerHTML += ` <div class="col-md-3 p-4 m-4 bg-warning"><p>${strMeal}</p>   <img class="malzeme" style=width:200px  src="${strMealThumb}" class="card-img-top" ></div>`;
+  });
+};
+
+//Yemek arama
+
+function searchFood() {
+  inputBox.addEventListener("input", () => {
+    //Matching control
+    display.innerHTML = "";
+    const myNewFilteredArray = myArray.filter((e) => {
+      return e.strMeal.toLowerCase().includes(inputBox.value.toLowerCase());
+    });
+    pressDisplay(myNewFilteredArray);
+  });
+}
