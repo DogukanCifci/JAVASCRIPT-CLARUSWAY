9b7eb658d4f559081aca073af1ccf3e5d073c27c
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-10 12:23:37 +0200

    OVERLOADING Added

--- class-notes/DE-16-OOP/4-Polymorpishm.js
+++ class-notes/DE-16-OOP/4-Polymorpishm.js
@@ -6,7 +6,7 @@
 
 //* Polymorphism  object-oriented programming concept li bir programlama kavramıdır,bir variable ın, nesnenin, yada fonksiyonun,birden çok form alma özelliğini ifade eder
 
-//* In the OOP, Polymorphism genellikle  Overloading and Overriding le gerçekleştirilir (overriding=aynı ada sahip iki işlev tanımlarsanız, tanımlanan sonuncusu önceden tanımlanmış sürümü geçersiz kılar ve işleve her çağrı yapıldığında, son tanımlanan işlev yürütülür.)(overloading=aşırı yükleme demektir, javascriptte böyle denmez, aynı adlarla fonksiyonu işlemek denir ve js  direk overloading i desteklemez, manuel uğraşmak gerekir )
+//* In the OOP, Polymorphism genellikle  Overloading and Overriding le gerçekleştirilir (overriding=aynı ada sahip iki işlev tanımlarsanız, tanımlanan sonuncusu önceden tanımlanmış sürümü geçersiz kılar ve işleve her çağrı yapıldığında, son tanımlanan işlev yürütülür.)(overloading=Overkoading'iemektir, javascriptte böyle denmez, aynı adlarla fonksiyonu işlemek denir ve js  direk overloading i desteklemez, manuel uğraşmak gerekir )
 
 class Book {
   constructor(a, b, c) {
@@ -31,6 +31,7 @@ class Dergi extends Book {
   //overriding yapacagimiz fonksiyonu buraya yazariz.
   ozetFunction() {
     return `Kitap Adi : ${this.title} Yazari : ${this.author} Kitap Basim Tarihi : ${this.year}`;
+    //Artik dergi classiyla olusturulmus bir sey cagirirsam ozetfunction yeni haliyle gözükecek
   }
 }
 
@@ -38,3 +39,27 @@ const dergi1 = new Dergi("Dogukan", "Leman Dergisi", 2022, "Eylül");
 
 console.log(dergi1);
 console.log(dergi1.ozetFunction());
+
+//
+//===================OVERLOADİNG===================
+// Diğer programlama dillerinden farklı olarak JavaScript , Function Overloading'i desteklemez .
+// İşte JavaScript'in İşlev Overkoading'i desteklemediğini gösteren küçük bir kod.
+
+function foo(arg1) {
+  console.log(arg1);
+}
+//Ayni fonskiyonu birden fazla defa tanimlarsak artik öncekilerin hicbir anlami yoktur. Hepsi silinmis gibi algilanir ve sadece en sonuncusu calisir
+
+function foo(arg1, arg2) {
+  console.log(arg1, arg2);
+}
+
+// Driver code
+foo("Geeks"); //Geeks undefined ---> Ciktisi
+//! Çıktı:
+//!Geeks tanımsız
+// Çıktıdaki "tanımsız" ifadesinin nedeni şudur: JavaScript'te aynı adla iki işlev tanımlanırsa, son tanımlanan işlev önceki işlevin üzerine yazar.
+// Bu durumda, foo(arg1) öğesinin üzerine foo(arg1,arg2) tarafından yazılmıştır, ancak
+// işleve yalnızca bir Argüman ("Geeks") ilettik. Bu, ikinci argümanın tanımsız olduğu anlamına gelir, yani ikinci argümanı yazdırmaya çalıştığımızda, "tanımsız" olarak yazdırılır.
+// Overkoading'işlevinin JavaScript'te desteklenmediğini gördük, ancak Overkoading'işlevini kendi başımıza uygulayabiliriz; bu, daha fazla sayıda ve daha fazla türde argüman söz konusu olduğunda oldukça karmaşıktır. Aşağıdaki kod, JavaScript'te Overkoading'işlevinin nasıl uygulanacağını anlamanıza yardımcı olacaktır.
+//*https://www.geeksforgeeks.org/function-overloading-in-javascript/
